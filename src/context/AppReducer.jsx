89678f6752b5ab4ef440c38f10d53c3e969c6746
ArchyScript <yungdansa@gmail.com>
2023-10-18 02:51:21 +0100

    ...

---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,16 @@
+export default (state, action) => {
+  if (action.type === 'DELETE_TRANSACTION') {
+    return {
+      ...state,
+      transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+    };
+  } else if (action.type === 'ADD_TRANSACTION') {
+    return {
+      ...state,
+      transactions: [action.payload, ...state.transactions]
+    };
+  }
+
+
+  return state;
+} 
