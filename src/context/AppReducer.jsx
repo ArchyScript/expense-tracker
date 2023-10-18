7b247ad879679c090c392cb6b0bee48b5a4b1335
ArchyScript <yungdansa@gmail.com>
2023-10-18 06:23:50 +0100

    fixed src/context/AppReducer.jsx

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,16 +1,32 @@
-export default (state, action) => {
-  if (action.type === 'DELETE_TRANSACTION') {
-    return {
-      ...state,
-      transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-    };
-  } else if (action.type === 'ADD_TRANSACTION') {
-    return {
-      ...state,
-      transactions: [action.payload, ...state.transactions]
-    };
+const AppReducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+      }
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      }
+    default:
+      return state;
   }
+  // if (action.type === 'DELETE_TRANSACTION') {
+  //   return {
+  //     ...state,
+  //     transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+  //   };
+  // } else if (action.type === 'ADD_TRANSACTION') {
+  //   return {
+  //     ...state,
+  //     transactions: [action.payload, ...state.transactions]
+  //   };
+  // }
 
 
-  return state;
-} 
+  // return state;
+}
+
+export default AppReducer
